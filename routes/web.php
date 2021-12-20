2591ed55b514d7d94961c80931001e4c83106380
tinsaemesfin <qmesfin@gmail.com>
2021-12-20 08:01:22 +0300

    end of ep 34

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}',function (Category $category){
     return view('posts', [
         'posts' => $category->posts,
-        'current_category' => $category,
+        'currentCategory' => $category,
         'categories' =>Category::all()
 
     ]);
