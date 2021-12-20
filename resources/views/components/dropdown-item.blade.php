c0cfcfdb7f8352d4dcbbebbd6c129cebcad6f1c6
tinsaemesfin <qmesfin@gmail.com>
2021-12-20 09:17:53 +0300

    end of ep 35

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,11 +1,11 @@
-@props(['active'=>false])
+ @props(['active'=>false])
 
 @php
     $classes='block px-3 text-left text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300';
     if($active)$classes.=' bg-blue-500 text-white';
 @endphp
 
-<a 
-{{ $attributes(['class'=>$classes]) }}>
+<a {{ $attributes(['class'=>$classes]) }}> 
+{{-- {{ $attributes(['class'=>$classes]) }}> --}}
 {{ $slot }}
 </a>
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,10 +1,13 @@
 @props(['trigger'])
 
-<div x-data="{show:false}" @click.away="show=false">
-     <div @click="show=!show">
-         {{ $trigger }}
-     </div>
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display: none">
-      {{ $slot }}
+<div x-data="{ show: false }" @click.away="show = false">
+  {{-- Trigger --}}
+  <div @click="show =! show">
+    {{ $trigger }}
+  </div>
+    
+{{-- Dropdown --}}
+    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display:none">
+        {{ $slot }}
     </div>
 </div>
