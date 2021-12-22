0c6fd94d2c45de77af7adaddcfcd1d72d0a2b9d2
tinsamesfin <qmesfin@gmail.com>
2021-12-22 19:19:14 +0300

    Search The Meassy way end of Ep 37

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,14 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
  */
 
 Route::get('/', function () {
-    
+        $posts=Post::latest();
+    if(request('search')){
+        $posts->where('title','like','%'.request('search').'%')
+        ->orWhere('body','like','%'.request('search').'%');
+    }
 
     return view('posts', [ 
-        'posts' => Post::latest()->get(),
+        'posts' => $posts->get(),
         'categories' =>Category::all()
     ]);
 })->name('home');
