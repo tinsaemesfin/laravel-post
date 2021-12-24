61d56e1de6ad0750cad7df19b1d5b3982a74a502
tinsaemesfin <qmesfin@gmail.com>
2021-12-24 14:12:26 +0300

    End Ep 49: Login and Logout

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class,'index'])->name('home');
 Route::get('posts/{post:slug}',[PostController::class,'show']);
 
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy']);
